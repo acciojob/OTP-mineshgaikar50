74766432e3dd2068f6ac36594a32a8c880b6e6e8
Minesh <54360467+mineshgaikar50@users.noreply.github.com>
2024-11-19 20:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const codes = document.querySelectorAll(".code");
+
+  codes.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+      const value = e.target.value;
+      if (value && index < codes.length - 1) {
+        codes[index + 1].focus();
+      }
+    });
+
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Backspace") {
+        if (!input.value && index > 0) {
+          codes[index - 1].focus();
+        }
+      }
+    });
+  });
+});
