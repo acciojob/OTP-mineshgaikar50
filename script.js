64564fe09712b38f5c11579f7654d16eb0be5636
Minesh <54360467+mineshgaikar50@users.noreply.github.com>
2024-12-29 22:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-const codes = document.querySelectorAll('.code');
+const inputs = document.querySelectorAll('.code');
 
-// Focus on the first input when the page loads
-codes[0].focus();
-
-codes.forEach((input, index) => {
+inputs.forEach((input, index) => {
   input.addEventListener('input', (e) => {
-    // Only allow numbers
-    const value = e.target.value.replace(/\D/, '');
-    e.target.value = value;
-
-    // Move to the next input if current is filled
-    if (value && index < codes.length - 1) {
-      codes[index + 1].focus();
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   });
 
   input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      // Clear the input and focus the previous input
-      input.value = '';
-      if (index > 0) {
-        codes[index - 1].focus();
-      }
+    if (e.key === 'Backspace' && index > 0 && !input.value) {
+      inputs[index - 1].focus();
     }
   });
 });
